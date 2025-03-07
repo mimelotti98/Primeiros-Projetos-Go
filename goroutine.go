ece903e7c9de91a7d7f37c0a5d705526758fbc74
Milena Melotti <154458288+mimelotti98@users.noreply.github.com>
2025-03-07 20:45:46 -0300

    Add files via upload

---
+++ goroutine.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+	}
+}
+
+func main() {
+	go f(0)
+	var escreva string
+	fmt.Scanln(&escreva)
+}
