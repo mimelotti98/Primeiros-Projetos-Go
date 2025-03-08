c57701270d7c22a394966de377e19e47609f5669
Milena Melotti <154458288+mimelotti98@users.noreply.github.com>
2025-03-07 22:33:59 -0300

    Add files via upload

---
+++ projetoB3.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func pingar(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func pongar(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
+func imprimir(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func main() {
+	var c chan string = make(chan string)
+
+	go pingar(c)
+	go pongar(c)
+	go imprimir(c)
+
+	var escreva string
+	fmt.Scanln(&escreva)
+}
